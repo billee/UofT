08658cd98d3bc855cad395867f1b146763ccdd79
bill <billie.tt@gmail.com>
2023-11-25 19:41:10 -0500

    nov 25 2023

---
+++ UofTProject/resources/views/includes/tabs/budget-table.blade.php
@@ -0,0 +1,8 @@
+    <thead>
+        <th>Item</th>
+        <th>Amount and description</th>
+        <th>Total</th>
+        <th></th>
+    </thead>
+    <tbody>
+    </tbody>
--- UofTProject/resources/views/includes/tabs/budget.blade.php
+++ UofTProject/resources/views/includes/tabs/budget.blade.php
@@ -41,34 +41,79 @@
                                 </h2>
                                 <div id="collapseOne" class="accordion-collapse collapse">
                                     <div class="accordion-body">
-                                        <table id="myTable">
-                                            <thead>
-                                                <th>Item</th>
-                                                <th>Amount and description</th>
-                                                <th>Total</th>
-                                                <th></th>
-                                            </thead>
-                                            <tbody>
-                                                {{-- <tr>
-                                                    <td><input type="text" name="budget_travel_item-0" id="budget_travel_item-0"></td>
-                                                    <td><input type="text" name="budget_travel_amount_desc-0" id="budget_travel_amount_desc-0"></td>
-                                                    <td><input type="text" name="budget_travel_total-0" id="budget_travel_total-0"></td>
-                                                    <td><button>Delete</button></td>
-                                                </tr> --}}
-                                            </tbody>
+                                        <table id="TravelTable">
+                                            @include('includes.tabs.budget-table')
                                         </table>
-
                                         <div>
-                                            <button type="button" class="btn btn-primary budget-travel-btn">
+                                            <button type="button" type="background-color:blue" class="btn btn-primary budget-travel-btn">
                                                 Add
-                                              </button>
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <!-- Repeat for other sections -->
-                        </div>
 
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingtwo">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo">
+                                        Accommodation<i>based on double occupancy for students)</i>
+                                    </button>
+                                </h2>
+                                <div id="collapseTwo" class="accordion-collapse collapse">
+                                    <div class="accordion-body">
+                                        <table id="AccommodationTable">
+                                            @include('includes.tabs.budget-table')
+                                        </table>
+                                        <div>
+                                            <button type="button" type="background-color:blue" class="btn btn-primary budget-accommodation-btn">
+                                                Add
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingThree">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree">
+                                        Food
+                                    </button>
+                                </h2>
+                                <div id="collapseThree" class="accordion-collapse collapse">
+                                    <div class="accordion-body">
+                                        <table id="FoodTable">
+                                            @include('includes.tabs.budget-table')
+                                        </table>
+                                        <div>
+                                            <button type="button" type="background-color:blue" class="btn btn-primary budget-food-btn">
+                                                Add
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingFour">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour">
+                                        Other funds received or applied for
+                                    </button>
+                                </h2>
+                                <div id="collapseFour" class="accordion-collapse collapse">
+                                    <div class="accordion-body">
+                                        <table id="FundTable">
+                                            @include('includes.tabs.budget-table')
+                                        </table>
+                                        <div>
+                                            <button type="button" type="background-color:blue" class="btn btn-primary budget-fund-btn">
+                                                Add
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
@@ -76,50 +121,12 @@
     </div>
 </div>
 
-
-
 <script type="text/javascript">
 
-    //var addButton = document.querySelector(".budget-travel-btn");
-
-    // addButton.addEventListener("click", function() {
-    //     var table = document.querySelector("#myTable tbody");
-    //     var rowCount = table.querySelectorAll("tr").length;
-
-    //     var row = document.createElement("tr");
-
-    //     var itemCell = document.createElement("td");
-    //     var itemInput = document.createElement("input");
-    //     itemInput.type = "text";
-    //     itemInput.name = "budget_travel_item-" + rowCount;
-    //     itemInput.id = "budget_travel_item-" + rowCount;
-    //     itemCell.appendChild(itemInput);
-
-    //     var amountCell = document.createElement("td");
-    //     var amountInput = document.createElement("input");
-    //     amountInput.type = "text";
-    //     amountInput.name = "budget_travel_amount_desc-" + rowCount;
-    //     amountInput.id = "budget_travel_amount_desc-" + rowCount;
-    //     amountCell.appendChild(amountInput);
-
-    //     var totalCell = document.createElement("td");
-    //     var totalInput = document.createElement("input");
-    //     totalInput.type = "text";
-    //     totalInput.name = "budget_travel_total-" + rowCount;
-    //     totalInput.id = "budget_travel_total-" + rowCount;
-    //     totalCell.appendChild(totalInput);
-
-    //     row.appendChild(itemCell);
-    //     row.appendChild(amountCell);
-    //     row.appendChild(totalCell);
-
-    //     table.appendChild(row);
-    // });
-
-    var addButton = document.querySelector(".budget-travel-btn");
-
-    addButton.addEventListener("click", function() {
-        var table = document.querySelector("#myTable tbody");
+    // travel
+    var addTravelButton = document.querySelector(".budget-travel-btn");
+    addTravelButton.addEventListener("click", function() {
+        var table = document.querySelector("#TravelTable tbody");
         var rowCount = table.querySelectorAll("tr").length;
 
         var row = document.createElement("tr");
@@ -162,8 +169,148 @@
         table.appendChild(row);
     });
 
+    //accommodation
+    var addAccommodationButton = document.querySelector(".budget-accommodation-btn");
+    addAccommodationButton.addEventListener("click", function() {
+        var table = document.querySelector("#AccommodationTable tbody");
+        var rowCount = table.querySelectorAll("tr").length;
+
+        var row = document.createElement("tr");
+
+        var itemCell = document.createElement("td");
+        var itemInput = document.createElement("input");
+        itemInput.type = "text";
+        itemInput.name = "budget_accommodation_item-" + rowCount;
+        itemInput.id = "budget_accommodation_item-" + rowCount;
+        itemCell.appendChild(itemInput);
+
+        var amountCell = document.createElement("td");
+        var amountInput = document.createElement("input");
+        amountInput.type = "text";
+        amountInput.name = "budget_accommodation_amount_desc-" + rowCount;
+        amountInput.id = "budget_accommodation_amount_desc-" + rowCount;
+        amountCell.appendChild(amountInput);
+
+        var totalCell = document.createElement("td");
+        var totalInput = document.createElement("input");
+        totalInput.type = "text";
+        totalInput.name = "budget_accommodation_total-" + rowCount;
+        totalInput.id = "budget_accommodation_total-" + rowCount;
+        totalCell.appendChild(totalInput);
+
+        var deleteCell = document.createElement("td");
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function() {
+            table.removeChild(row);
+            reindexRows();
+        });
+        deleteCell.appendChild(deleteButton);
+
+        row.appendChild(itemCell);
+        row.appendChild(amountCell);
+        row.appendChild(totalCell);
+        row.appendChild(deleteCell);
+
+        table.appendChild(row);
+    });
+
+    //food
+    var addFoodButton = document.querySelector(".budget-food-btn");
+    addFoodButton.addEventListener("click", function() {
+        var table = document.querySelector("#FoodTable tbody");
+        var rowCount = table.querySelectorAll("tr").length;
+
+        var row = document.createElement("tr");
+
+        var itemCell = document.createElement("td");
+        var itemInput = document.createElement("input");
+        itemInput.type = "text";
+        itemInput.name = "budget_food_item-" + rowCount;
+        itemInput.id = "budget_food_item-" + rowCount;
+        itemCell.appendChild(itemInput);
+
+        var amountCell = document.createElement("td");
+        var amountInput = document.createElement("input");
+        amountInput.type = "text";
+        amountInput.name = "budget_food_amount_desc-" + rowCount;
+        amountInput.id = "budget_food_amount_desc-" + rowCount;
+        amountCell.appendChild(amountInput);
+
+        var totalCell = document.createElement("td");
+        var totalInput = document.createElement("input");
+        totalInput.type = "text";
+        totalInput.name = "budget_food_total-" + rowCount;
+        totalInput.id = "budget_food_total-" + rowCount;
+        totalCell.appendChild(totalInput);
+
+        var deleteCell = document.createElement("td");
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function() {
+            table.removeChild(row);
+            reindexRows();
+        });
+        deleteCell.appendChild(deleteButton);
+
+        row.appendChild(itemCell);
+        row.appendChild(amountCell);
+        row.appendChild(totalCell);
+        row.appendChild(deleteCell);
+
+        table.appendChild(row);
+    });
+
+    //other funds
+    var addFundButton = document.querySelector(".budget-fund-btn");
+    addFundButton.addEventListener("click", function() {
+        var table = document.querySelector("#FundTable tbody");
+        var rowCount = table.querySelectorAll("tr").length;
+
+        var row = document.createElement("tr");
+
+        var itemCell = document.createElement("td");
+        var itemInput = document.createElement("input");
+        itemInput.type = "text";
+        itemInput.name = "budget_fund_item-" + rowCount;
+        itemInput.id = "budget_fund_item-" + rowCount;
+        itemCell.appendChild(itemInput);
+
+        var amountCell = document.createElement("td");
+        var amountInput = document.createElement("input");
+        amountInput.type = "text";
+        amountInput.name = "budget_fund_amount_desc-" + rowCount;
+        amountInput.id = "budget_fund_amount_desc-" + rowCount;
+        amountCell.appendChild(amountInput);
+
+        var totalCell = document.createElement("td");
+        var totalInput = document.createElement("input");
+        totalInput.type = "text";
+        totalInput.name = "budget_fund_total-" + rowCount;
+        totalInput.id = "budget_fund_total-" + rowCount;
+        totalCell.appendChild(totalInput);
+
+        var deleteCell = document.createElement("td");
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function() {
+            table.removeChild(row);
+            reindexRows();
+        });
+        deleteCell.appendChild(deleteButton);
+
+        row.appendChild(itemCell);
+        row.appendChild(amountCell);
+        row.appendChild(totalCell);
+        row.appendChild(deleteCell);
+
+        table.appendChild(row);
+    });
+
+
+
     function reindexRows() {
-        var table = document.querySelector("#myTable tbody");
+        var table = document.querySelector("#TravelTable tbody");
         var rows = Array.from(table.querySelectorAll("tr"));
         rows.forEach(function(row, index) {
             var inputs = Array.from(row.querySelectorAll("input"));
@@ -174,7 +321,5 @@
             });
         });
     }
-
-
 
 </script>
